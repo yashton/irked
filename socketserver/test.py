9a9da09164189d01a180470e74a1c762c464825d
Ashton Snelgrove <yashton@gmail.com>
2012-01-22 17:51:26 -0700

    Parameterize the test script.

--- socketserver/test.py
+++ socketserver/test.py
@@ -4,17 +4,18 @@ import sys
 import time
 import random
 
-HOST, PORT = "localhost", 6667
+HOST, PORT = sys.argv[1], int(sys.argv[2])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
 try:
     while True:
-        datas = open(sys.argv[1])
+        datas = open(sys.argv[3])
         for data in datas:
             sock.send(bytes(data, "utf-8"))
             received = str(sock.recv(1024), "utf-8")
             time.sleep(0.1)
+        datas.close()
 finally:
     sock.close()
