e9a067bc3c34fd51a94fd9ea202d57b7387ea4ce
Ashton Snelgrove <yashton@gmail.com>
2012-01-29 14:57:42 -0700

    Add ignore rules for Mercurial.

--- asyncserver/async_echo.py
+++ asyncserver/async_echo.py
@@ -58,80 +58,90 @@ class EchoHandler(asyncore.dispatcher):
         LOGGER.debug('command=%s args=%s', command, args)
 
         if command == 'NICK':
-            # TODO: err irc.ERR_UNAVAILRESOURCE
-
-            if not len(args):
-                self._send(irc.ERR_NONICKNAMEGIVEN, ':No nickname given')
-                return
-
-            # TODO: validate nickname (irc.ERR_ERRONEUSNICKNAME)
-
-            nick = args[0]
-            if nick in self.server.names:
-                self._send(irc.ERR_NICKNAMEINUSE, '%s :Nickname is already in use' % self.nick)
-                return
-
-            # TODO: nickname collision (irc.ERR_NICKCOLLISION) -- multiple server stuff
-
-            # TODO: restricted (irc.ERR_RESTRICTED)
-            # i'm not really sure when it's best to lock the nick, freenode
-            # seems to not lock the nick until you're registered, so i guess
-            # that's how we should do it too
-            if self.registered:
-                old_nick = self.nick
-                # TODO: nick delay mechanism
-                self.server.names.remove(old_nick)
-                self.server.names.add(nick)
-
-            self.nick = nick
-
+            self.cmd_nick(args)
         elif command == 'USER':
-            if len(args) < 4:
-                self. _err_need_more_params(command)
-                return
-
-            if self.registered:
-                self._send(irc.ERR_ALREADYREGISTRED, ':Unauthorized command (already registered)')
-
-            user = args[0]
-            mode = args[1]
-            realname = str.join(" ", args[3:]) # NOTE: real command parser will fix this
-
-            self.user = user, mode, realname
-
-            # TODO? write a nick-changing method that checks for this nick (race condition?)
-            self.server.names.add(self.nick) 
-            self.registered = True
-            self._send(irc.RPL_WELCOME, 'Welcome to the Internet Relay Network %s' % self.nick)
-            self._send(irc.RPL_YOURHOST, 'Your host is FIXME, running version FIXME')
-            self._send(irc.RPL_CREATED, 'This server was created FIXME')
-            self._send(irc.RPL_MYINFO, 'FIXMEservername FIXMEversion FIXMEusemodes FIXMEchannelmodes')
-
-            # TODO: show motd
-
+            self.cmd_user(args)
         elif command == 'JOIN':
-            # TODO: support multiple channels at once
-            # TODO: support leaving all channels ('0')
-            # TODO: support keys
-            channel = args[0]
-
-            if channel not in self.server.channels:
-              self.server.channels[channel] = Channel(channel, server)
-            self.server.channels[channel].add(self)
-
+            self.cmd_join(args)
         elif command == 'QUIT':
-            self.server.clients.remove(self)
-
-            # TODO: notify relevant servers/clients
-
-            if self.registered:
-                self.server.names.remove(self.nick)
-
-            # TODO: send error message (see RFC)
-            self.close()
+            self.cmd_quit(args)
         else:
-            # raise an error
+            LOGGER.error("Unknown command %s with args %s", command, args)
             pass
+
+    def cmd_nick(self, args):
+        # TODO: err irc.ERR_UNAVAILRESOURCE
+        if not len(args):
+            self._send(irc.ERR_NONICKNAMEGIVEN, ':No nickname given')
+            return
+
+        # TODO: validate nickname (irc.ERR_ERRONEUSNICKNAME)
+
+        nick = args[0]
+        if nick in self.server.names:
+            self._send(irc.ERR_NICKNAMEINUSE, '%s :Nickname is already in use' % self.nick)
+            return
+
+        # TODO: nickname collision (irc.ERR_NICKCOLLISION) -- multiple server stuff
+
+         # TODO: restricted (irc.ERR_RESTRICTED)
+        # i'm not really sure when it's best to lock the nick, freenode
+        # seems to not lock the nick until you're registered, so i guess
+        # that's how we should do it too
+        if self.registered:
+            old_nick = self.nick
+            # TODO: nick delay mechanism
+            self.server.names.remove(old_nick)
+            self.server.names.add(nick)
+        self.nick = nick
+
+    def cmd_user(self, args):
+        if len(args) < 4:
+            self. _err_need_more_params(command)
+            return
+
+        if self.registered:
+            self._send(irc.ERR_ALREADYREGISTRED, ':Unauthorized command (already registered)')
+
+        user = args[0]
+        mode = args[1]
+        realname = str.join(" ", args[3:]) # NOTE: real command parser will fix this
+
+        self.user = user, mode, realname
+
+        # TODO? write a nick-changing method that checks for this nick (race condition?)
+        self.server.names.add(self.nick) 
+        self.registered = True
+        self._send(irc.RPL_WELCOME, 'Welcome to the Internet Relay Network %s' % self.nick)
+        self._send(irc.RPL_YOURHOST, 'Your host is FIXME, running version FIXME')
+        self._send(irc.RPL_CREATED, 'This server was created FIXME')
+        self._send(irc.RPL_MYINFO, 'FIXMEservername FIXMEversion FIXMEusemodes FIXMEchannelmodes')
+            
+        # TODO: show motd
+
+
+    def cmd_join(self, args):
+        # TODO: support multiple channels at once
+        # TODO: support leaving all channels ('0')
+        # TODO: support keys
+        channel = args[0]
+
+        if channel not in self.server.channels:
+            self.server.channels[channel] = Channel(channel, server)
+        self.server.channels[channel].add(self)
+
+    def cmd_quit(self, args):
+        self.server.clients.remove(self)
+        
+        # TODO: notify relevant servers/clients
+        
+        if self.registered:
+            self.server.names.remove(self.nick)
+
+        # TODO: send error message (see RFC)
+        self.close()
+        
+
 
         LOGGER.debug(repr(self.server))
 
